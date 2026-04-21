2bd53527cc5aeecc373b0198a35c654329395ee8
khushisahu4544 <khushisahu4544@gmail.com>
2026-04-21 20:12:39 +0530

    added interview page,category selection and navigation

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function start(){
     alert("Interview Started");
 }
+// START BUTTON
+function start() {
+  let category = document.getElementById("category").value;
+
+  // Save selected category
+  localStorage.setItem("category", category);
+
+  // Go to next page
+  window.location.href = "interview.html";
+}
+
+// BACK BUTTON
+function goBack() {
+  window.location.href = "index.html";
+}
