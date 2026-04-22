0f68fe9789ec7f6b97e63d8d4594be59d97ddc67
khushisahu4544 <khushisahu4544@gmail.com>
2026-04-22 16:43:57 +0530

    Enhanced UI forinterview page with iput styling

---
+++ README.md
@@ -0,0 +1,28 @@
+#  InterviewPrep AI
+
+A simple web application to practice interview skills.
+
+---
+
+## Features
+- Category selection (JavaScript / HR)
+- Multi-page navigation
+- Clean UI design
+
+---
+
+##  Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+---
+
+##  Pages
+- index.html → Home page
+- interview.html → Interview page
+
+---
+
+##  Purpose
+This project helps users practice interview questions and improve confidence.
--- script.js
+++ script.js
@@ -16,3 +16,54 @@ function start() {
 function goBack() {
   window.location.href = "index.html";
 }
+// DIFFERENT QUESTION SETS
+let jsQuestions = [
+  "What is JavaScript?",
+  "What is closure?",
+  "Difference between var, let, const?"
+];
+
+let hrQuestions = [
+  "Tell me about yourself",
+  "Why should we hire you?",
+  "What are your strengths?"
+];
+
+let questions = [];
+let index = 0;
+
+// LOAD QUESTIONS BASED ON CATEGORY
+window.onload = function () {
+
+  let category = localStorage.getItem("category");
+
+  if (category === "js") {
+    questions = jsQuestions;
+  } else {
+    questions = hrQuestions;
+  }
+
+  document.getElementById("question").innerText = questions[index];
+};
+
+// NEXT BUTTON
+function nextQuestion() {
+  let userAnswer = document.getElementById("answer").value;
+
+  console.log("User Answer:", userAnswer);
+
+  index++;
+
+  if (index < questions.length) {
+    document.getElementById("question").innerText = questions[index];
+    document.getElementById("answer").value = "";
+  } else {
+    document.getElementById("question").innerText = "🎉 Interview Completed!";
+    document.getElementById("answer").style.display = "none";
+  }
+}
+
+// BACK BUTTON
+function goBack() {
+  window.location.href = "index.html";
+}
